76bae4c87dd751ead2a4ce6fad5c6884c6da443c
Tommy McCarthy <thomasmccarthy@college.harvard.edu>
2018-06-14 16:26:58 -0400

    registration fixes

--- src/pages/edit/edit.ts
+++ src/pages/edit/edit.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Http, Headers } from "@angular/http";
 
 import { HomePage } from '../home/home';
 import { ProfilePage } from '../profile/profile';
 import { User } from '../../models/user';
 
+/**
+ * Generated class for the EditPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
+@IonicPage()
 @Component({
-  selector: 'page-register',
-  templateUrl: 'register.html'
+  selector: 'page-edit',
+  templateUrl: 'edit.html',
 })
-export class RegisterPage {
+export class EditPage {
 
   public user: User = new User();
 
-  constructor(public navCtrl: NavController, public http: Http) {
+  constructor(public navCtrl: NavController, public http: Http, public navParams: NavParams) {
+    this.user = this.navParams.get("user");
   }
 
   navigateToHome() {
@@ -24,7 +32,7 @@ export class RegisterPage {
     });
   }
 
-  register() {
+  edit() {
     this.http
       .post("http://localhost:3000/register", {
         firstname: this.user.firstname,
@@ -34,13 +42,10 @@ export class RegisterPage {
       })
       .subscribe(
         result => {
-          let token = result.json().token;
+          console.log(result);
 
           // Our username and password (on this) should have data from the user
-          this.navCtrl.push(HomePage, {
-              user: this.user,
-              token,
-          });
+          this.navCtrl.push(HomePage, {});
         },
 
         error => {
