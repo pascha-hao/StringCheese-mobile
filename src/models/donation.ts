f63d46355e71f22fb3e371652c9672e699acbf5a
Tommy McCarthy <thomasmccarthy@college.harvard.edu>
2018-06-20 15:22:29 -0400

    working history

---
+++ src/models/donation.ts
@@ -0,0 +1,8 @@
+import { DateTime } from "ionic-angular";
+
+export class Donation {
+    charity_id: number;
+    amount: number;
+    charity_name: string;
+    //donate_date: DateTime;
+}
--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -4,6 +4,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { User } from '../../models/user';
 import { Charity } from '../../models/charityProfile';
 import { BrowsePage } from '../browse/browse';
+import { DonationsPage } from '../donations/donations';
 import { StripeNativePage } from '../stripe-native/stripe-native';
 import { StripeJavaScriptPage } from '../stripe-java-script/stripe-java-script';
  
@@ -33,17 +34,30 @@ export class PaymentPage {
     this.user = this.navParams.get("user");
     this.charity = this.navParams.get("charity");
     this.jwt = localStorage.getItem('jwt')
-    this.http 
-    .get("http://localhost:3000/users", {
-      params: {
-        jwt: this.jwt
-      }
-    })
+  
+    this.http
+      .get("http://localhost:3000/users", {
+        params: {
+          jwt: this.jwt
+        }
+      })
+      .subscribe(
+        result => {
+          let newUser = result.json().user;
+          this.user = newUser;
+        },
+        error => {
+          console.log(error);
+        }
+        
+      );
+    }
+   
       // .post('/donations'), {
       //     amount: this.payment,
       //     date: this.
       // }
-  }
+  
   openJavaScript(){
     this.navCtrl.push(StripeJavaScriptPage)
   }
@@ -66,11 +80,29 @@ export class PaymentPage {
     console.log('ionViewDidLoad PaymentPage');
   }
 
-  navigateToBrowse() {
-    this.navCtrl.push(BrowsePage, {
-      user: this.user,
-      payment: this.payment
-    });
+  navigateToPortfolio() {
+    console.log(this.charity.id);
+    console.log(this.user.id);
+    this.http
+      .post("http://localhost:3000/donation", {
+        charity_id: this.charity.id,
+        user_id: this.user.id,
+        amount: this.payment,
+        charity_name: this.charity.name,
+        //donate_date: Date.now()
+      })
+      .subscribe(
+        result => {
+          console.log("whats going on")
+          this.navCtrl.push(DonationsPage, {
+              user: this.user
+          });
+        },
+
+        error => {
+          console.log(error);
+        }
+      );
   }
 
 }
