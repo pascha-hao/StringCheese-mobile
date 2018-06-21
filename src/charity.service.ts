b5deafee83f75ca4bf3447565d97703fc058e4c2
eae4bf <35538100+eae4bf@users.noreply.github.com>
2018-06-21 13:52:30 +0200

    searchbar

---
+++ src/charity.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Charity } from '../../stringcheese-mobile/src/models/charityProfile';
+import { Observable } from 'rxjs/Observable';
+// import { ArrayObservable } from 'rxjs/observable/ArrayObservable'
+import { Storage } from '@ionic/storage';
+import { Http } from "@angular/http";
+import { ConfigService } from '../src/config.service';
+import 'rxjs/add/observable/of';
+
+@Injectable()
+export class CharityServiceProvider {
+
+  private charities: Array<Charity> = []; 
+
+  constructor(
+      public http:Http,
+      public configService: ConfigService  
+  ) {
+    this.http
+    .get(this.configService.getBaseUrl() + "/charities")
+    .subscribe(
+      result => {
+        let i = 0;
+        while (i < result.json().length) {
+          this.charities.push(result.json()[i]);
+          i++;
+        }   
+      },
+      error => {
+        console.log(error);
+      }
+    ); 
+  }
+
+  filterCharities(searchText){
+    return this.charities.filter((charity) => {
+        return charity.name.toLowerCase().indexOf(searchText.toLowerCase()) > -1;
+    });    
+
+}
+
+//   getFilteredCharites(searchText: string): Observable<Charity[]> {
+//     return Observable.create( observable => {
+//         this.getCharities().subscribe(allCharities => {
+//             if(searchText && searchText.length > 0) {
+//                 let filteredCharities = allCharities
+//                     .filter(j => j.name.toLowerCase().indexOf(searchText.toLowerCase()) > -1)
+//                     observable.next(filteredCharities);
+//             }
+//             else {
+//                 observable.next(allCharities);
+//             }
+//             observable.complete();
+//             });
+//         });
+//     }
+
+//   getCharities() :Observable<Charity[]> {
+//     // return Observable.of(this.charities);
+//     return this.charities;
+//   }
+
+}
--- src/pages/browse/browse.ts
+++ src/pages/browse/browse.ts
@@ -3,9 +3,12 @@ import { NavController, NavParams } from 'ionic-angular';
 import { CharityPage } from '../charity/charity';
 import { ProfilePage } from '../profile/profile';
 import { Charity } from '../../models/charityProfile';
+import { FormControl } from '@angular/forms';
 import { User } from '../../models/user';
 import { Http } from '@angular/http';
 import { ConfigService } from '../../config.service';
+import { CharityServiceProvider } from '../../charity.service'
+import 'rxjs/add/operator/debounceTime';
 
 /**
  * Generated class for the BrowsePage page.
@@ -18,40 +21,68 @@ import { ConfigService } from '../../config.service';
   selector: 'page-browse',
   templateUrl: 'browse.html',
 })
-  export class BrowsePage {
+export class BrowsePage {
 
-  public isSearchbarOpened= false;
-
+  public isSearchbarOpened = false;
+  public searchText: string = '';
+  public searchControl: FormControl;
   public charities: Array<Charity> = [];
   public user: User = new User();
+  public searching: any = false;
 
   constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams, 
-    public http: Http, 
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public http: Http,
+    public charityService: CharityServiceProvider,
     public configService: ConfigService) {
+    this.searchControl = new FormControl();
     this.user = this.navParams.get("user");
     this.http
-    .get(this.configService.getBaseUrl() + "/charities")
-    .subscribe(
-      result => {
-        let i=0;
-        while(i<result.json().length){
-          this.charities.push(result.json()[i]);
-          i++;
+      .get(this.configService.getBaseUrl() + "/charities")
+      .subscribe(
+        result => {
+          let i = 0;
+          while (i < result.json().length) {
+            this.charities.push(result.json()[i]);
+            i++;
+          }
+        },
+        error => {
+          console.log(error);
         }
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  
+      );
+
 
   }
+
+  // search() {
+  //   this.charityService.getFilteredCharites(this.searchText)
+  //     .subscribe(filteredCharities => {
+  //       this.charities = filteredCharities;
+  //     });
+  // }
 
   ionViewDidLoad() {
+    this.setFilteredCharities();
+
+    this.searchControl.valueChanges.debounceTime(700).subscribe(search => {
+      this.searching = false;
+      this.setFilteredCharities();
+
+    });
+
     console.log('ionViewDidLoad BrowsePage');
   }
+
+  onSearchInput() {
+    this.searching = true;
+  }
+
+  setFilteredCharities() {
+    this.charities = this.charityService.filterCharities(this.searchText);
+  }
+
 
   navigateToCharity(charity: Charity) {
     this.navCtrl.push(CharityPage, {
