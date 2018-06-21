91df7d446c6e2107f55c9bba2934a91b5e881e39
eae4bf <35538100+eae4bf@users.noreply.github.com>
2018-06-21 21:55:59 +0200

    projects updated, charity and browse page

--- src/pages/charity/charity.ts
+++ src/pages/charity/charity.ts
@@ -6,7 +6,9 @@ import { Charity } from '../../models/charityProfile';
 import { DonationsPage } from '../donations/donations';
 import { PaymentPage } from '../payment/payment';
 import { StripeJavaScriptPage } from '../stripe-java-script/stripe-java-script';
-
+import { Http } from '@angular/http';
+import { ConfigService } from '../../config.service';
+import { Project } from '../../models/project';
 /**
  * Generated class for the CharityPage page.
  *
@@ -22,12 +24,51 @@ export class CharityPage {
 
     public user: User = new User();
     public charity: Charity = new Charity();
+    public projects: Array<Project> = [];
+    public charProjects: Array <Project> = [];
+    public projectsExist: boolean;
 
-
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    constructor(
+      public navCtrl: NavController, 
+      public navParams: NavParams, 
+      public http: Http,
+      public configService: ConfigService
+    ) {
       this.charity = this.navParams.get("charity");
       this.user = this.navParams.get("user");
+      this.http
+        .get(this.configService.getBaseUrl() + "/projects")
+        .subscribe(
+          result => {
+            let i = 0;
+            while (i < result.json().length) {
+              this.projects.push(result.json()[i]);
+              i++;
+            }
+            for(let j=0; j<this.projects.length; j++) {
+              if(this.projects[j].charity_id === this.charity.id) {
+                this.charProjects.push(this.projects[j]);
+              }
+            }
+            if(this.charProjects.length > 0){
+              var exist = true;
+            }
+            else {
+              var exist = false;
+            }
+            // if(!exist) {
+
+            // }
+          }, 
+          error => {
+            console.log(error);
+          }
+        )
     }
+
+
+
+
 
   navigateToCharity(item) {
     this.navCtrl.push(CharityPage, {
