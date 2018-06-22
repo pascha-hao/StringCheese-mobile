8beabeba1fcd4bda60a5010dfded0e68603eb25c
Tommy McCarthy <thomasmccarthy@college.harvard.edu>
2018-06-21 22:04:20 -0400

    almost working favorites page

---
+++ src/models/myfavs.ts
@@ -0,0 +1,5 @@
+export class MyFavs {
+    id: number;
+    user_id: number;
+    charity_id: number;
+}
--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -1,21 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
-import { User } from '../../models/user';
+import { CharityPage } from '../charity/charity';
+import { ProfilePage } from '../profile/profile';
 import { Charity } from '../../models/charityProfile';
-import { BrowsePage } from '../browse/browse';
-import { DonationsPage } from '../donations/donations';
-import { TotalsPage } from '../totals/totals';
-import { BreakdownPage } from '../breakdown/breakdown';
-import { StripeNativePage } from '../stripe-native/stripe-native';
-import { StripeJavaScriptPage } from '../stripe-java-script/stripe-java-script';
+import { FormControl } from '@angular/forms';
+import { User } from '../../models/user';
+import { MyFavs } from '../../models/myfavs';
+import { Http } from '@angular/http';
 import { ConfigService } from '../../config.service';
-import { Http } from "@angular/http";
-//import { DonationsPage } from '../donations/donations';
-//import { ProfilePage } from '../profile/profile';
+import { CharityServiceProvider } from '../../charity.service'
+import 'rxjs/add/operator/debounceTime';
 
 /**
- * Generated class for the PaymentPage page.
+ * Generated class for the BrowsePage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -27,18 +24,24 @@ import { Http } from "@angular/http";
 })
 export class PaymentPage {
 
+  public isSearchbarOpened = false;
+  public searchText: string = '';
+  public searchControl: FormControl;
+  public tempcharities: Array<Charity> = [];
+  public charities: Array<Charity> = [];
   public user: User = new User();
-  public charity: Charity = new Charity();
-  public payment: number;
+  public searching: any = false;
+  public myfavs: Array<MyFavs> = [];
+  public charityids: Array<number> = [];
   jwt: string;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public http: Http,
+    public charityService: CharityServiceProvider,
     public configService: ConfigService) {
-    this.user = this.navParams.get("user");
-    this.charity = this.navParams.get("charity");
+    this.searchControl = new FormControl();
     this.jwt = localStorage.getItem('jwt')
 
     this.http
@@ -51,71 +54,86 @@ export class PaymentPage {
         result => {
           let newUser = result.json().user;
           this.user = newUser;
+          this.http
+
+            .get(this.configService.getBaseUrl() + "/myfavorites?user_id=" + this.user.id, {
+            })
+            .subscribe(
+              result => {
+                this.myfavs = result.json();
+                for (var i = 0; i < this.myfavs.length; i++) {
+                  this.charityids.push(this.myfavs[i].charity_id)
+                }
+                this.http
+                  .get(this.configService.getBaseUrl() + "/charities")
+                  .subscribe(
+                    result => {
+                      let i = 0;
+                      while (i < result.json().length) {
+                        this.tempcharities.push(result.json()[i]);
+                        i++;
+                      }
+                      for (var j = 0; j < this.charityids.length; j++) {
+                        if (this.tempcharities[j].id == this.charityids[j]) {
+                          this.charities.push(this.tempcharities[j]);
+                        }
+                      }
+                    },
+                    error => {
+                      console.log(error);
+                    }
+                  );
+              },
+              error => {
+                console.log(error);
+              }
+            );
         },
         error => {
           console.log(error);
         }
-
       );
   }
-
-
-  openJavaScript() {
-    this.navCtrl.push(StripeJavaScriptPage)
-  }
-  openNative() {
-    this.navCtrl.push(StripeNativePage)
-  }
-
-  // createDonation() {
-  //   var val = localStorage.getItem('jwt');
-  //   this.jwt = val;
-
-
-
-
-  // }
-
 
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad PaymentPage');
+    this.setFilteredCharities();
+
+    this.searchControl.valueChanges.debounceTime(700).subscribe(search => {
+      this.searching = false;
+      this.setFilteredCharities();
+
+    });
+
+    console.log('ionViewDidLoad BrowsePage');
   }
 
-  navigateToBrowse() {
-    this.navCtrl.push(BrowsePage, {
-      user: this.user,
-      payment: this.payment
+  onSearchInput() {
+    this.searching = true;
+  }
+
+  setFilteredCharities() {
+    this.charities = this.charityService.filterCharities(this.searchText);
+  }
+
+
+  navigateToCharity(charity: Charity) {
+    this.navCtrl.push(CharityPage, {
+      charity: charity,
+      user: this.user
     });
   }
 
-  navigateToPortfolio() {
-    console.log(this.charity.id);
-    console.log(this.user.id);
-    this.http
-      .post(this.configService.getBaseUrl() + "/donation", {
-        charity_id: this.charity.id,
-        user_id: this.user.id,
-        amount: this.payment,
-        charity_name: this.charity.name,
-        //donate_date: Date.now()
-      })
-      .subscribe(
-        result => {
-          console.log("whats going on")
-          this.navCtrl.push(TotalsPage, {
-              user: this.user
-          });
-        },
-
-        error => {
-          console.log(error);
-        }
-      );
+  navigateToProfile() {
+    this.navCtrl.push(ProfilePage, {
+      user: this.user
+    });
   }
 
+  navigateToBrowse(charity: Charity) {
+    this.navCtrl.push(CharityPage, {
+      charity: charity,
+      user: this.user
+    });
+  }
 }
-
-
-
-
